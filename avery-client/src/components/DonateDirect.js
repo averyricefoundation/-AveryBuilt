0a0b3e395b46c07e9e340b659d7e64093cdf9f50
unknown <andrescardenas34@outlook.com>
2019-04-14 22:29:47 -0400

    yo

--- avery-client/src/components/DonateDirect.js
+++ avery-client/src/components/DonateDirect.js
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
 import "./DonateDirectly.css"
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+
 
 export default class DonateDirectly extends Component {
     render(){
+        
+		const onSuccess = (payment) => {
+			// 1, 2, and ... Poof! You made it, everything's fine and dandy!
+            		console.log("Payment successful!", payment);
+            		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+		}
+
+		const onCancel = (data) => {
+			// The user pressed "cancel" or closed the PayPal popup
+			console.log('Payment cancelled!', data);
+			// You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+		}
+
+		const onError = (err) => {
+			// The main Paypal script could not be loaded or something blocked the script from loading
+			console.log("Error!", err);
+			// Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+			// => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+		}
+
+		let env = 'sandbox'; // you can set this string to 'production'
+		let currency = 'USD'; // you can set this string from your props or state  
+		// Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
+
+        const client = {
+            sandbox:process.env.REACT_APP_PAYPAL_API_KEY,
+            production: 'YOUR-PRODUCTION-APP-ID',
+        }  
         return (
+
             <div className="container text-center">
                     <h2 className="mt-5 thankyou">You're contribution is greatly apprechiated.</h2>
                     <div className="row mt-5">
@@ -12,7 +43,7 @@ export default class DonateDirectly extends Component {
                             <div className="card-body">
                                 <h1 className="card-title">$5</h1>
                                 <p className="card-text">Click below to donate.</p>
-                                <a href="#" className="btn btn-outline-warning">Donate $5</a>
+                                <PaypalExpressBtn env={env} client={client} currency={currency} total={5.00} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
                             </div>
                             </div>
                         </div>
@@ -21,7 +52,7 @@ export default class DonateDirectly extends Component {
                             <div className="card-body">
                                 <h1 className="card-title">$10</h1>
                                 <p className="card-text">Click below to donate.</p>
-                                <a href="#" className="btn btn-outline-warning">Donate $10</a>
+                                <PaypalExpressBtn env={env} client={client} currency={currency} total={10.00} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
                             </div>
                             </div>
                         </div>
@@ -33,7 +64,7 @@ export default class DonateDirectly extends Component {
                             <div className="card-body">
                                 <h1 className="card-title">$15</h1>
                                 <p className="card-text">Click below to donate.</p>
-                                <a href="#" className="btn btn-outline-warning">Donate $15</a>
+                                <PaypalExpressBtn env={env} client={client} currency={currency} total={15.00} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
                             </div>
                             </div>
                         </div>
@@ -42,7 +73,7 @@ export default class DonateDirectly extends Component {
                             <div className="card-body">
                                 <h1 className="card-title">$20</h1>
                                 <p className="card-text">Click below to donate.</p>
-                                <a href="#" className="btn btn-outline-warning">Donate $20</a>
+                                <PaypalExpressBtn env={env} client={client} currency={currency} total={20.00} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
                             </div>
                             </div>
                         </div>
